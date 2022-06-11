f8af85108868f95d9e81096d9b291ea9e026c2c8
khaled-11 <e.khaled.abouseada@gmail.com>
2022-06-11 11:56:15 -0400

    Start

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-// test('renders page', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/weather app/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
