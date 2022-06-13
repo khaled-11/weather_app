143c4f42de2d3e360c4999450850343a52151db1
khaled-11 <e.khaled.abouseada@gmail.com>
2022-06-13 00:18:53 -0400

    date update

--- src/forecast.js
+++ src/forecast.js
@@ -5,10 +5,8 @@ export default function Forecast(props){
     var forecastDataMap = new Map();
     var forecastTabs = [];
     for (let i = 0 ; i < props.forecastData.list.length; i++){
-      console.log(props.forecastData.list[i].dt_txt)
-      console.log(new Date(props.forecastData.list[i].dt*1000))
-      console.log(`${new Intl.DateTimeFormat('en-US', { days:'2-digit' ,hour: '2-digit', minute: '2-digit' }).format(props.forecastData.list[i].dt*1000)}_${new Date(props.forecastData.list[i].dt*1000)}`)
-      let day = new Intl.DateTimeFormat('en-US').format(props.forecastData.list[i].dt*1000)
+      let day = props.forecastData.list[i].dt_txt.split(" ")[0]
+      console.log(day)
       if (forecastDataMap.get(day)){
           forecastDataMap.set(day,[...forecastDataMap.get(day), props.forecastData.list[i]])
         } else {
@@ -18,17 +16,16 @@ export default function Forecast(props){
     forecastDataMap.forEach((data,dataKey)=>{
       forecastTabs.push(
         <Accordion.Item key={dataKey} eventKey={forecastTabs.length}>
-          <Accordion.Header>Forecast ({new Intl.DateTimeFormat('en-US').format(new Date(data[0].dt_txt.split(" ")[0]))})</Accordion.Header>
+          <Accordion.Header>Forecast ({data[0].dt_txt.split(" ")[0]})</Accordion.Header>
           <Accordion.Body>
             <Tabs defaultActiveKey={data[0].dt} className="mb-3">
               {
                 data.map((item)=>{
-                  return <Tab key={item.dt} eventKey={item.dt} title={new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit'}).format(item.dt*1000)}>
+                  return <Tab key={item.dt} eventKey={item.dt} title={item.dt_txt.split(" ")[1]}>
                     <WeatherCard weatherData={{current:item}}></WeatherCard>
                   </Tab>
                 })
               } 
-
             </Tabs>
           </Accordion.Body>
         </Accordion.Item>
